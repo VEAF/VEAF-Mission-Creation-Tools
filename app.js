1bf9774ab5990b258ffcbd834a642664ffbf582c
David Pierron <dpierron@gmail.com>
2020-05-09 11:33:23 +0200

    Refactor the build pipeline to make it dependent only on node.js
    wip

---
+++ app.js
@@ -0,0 +1,31 @@
+/*
+* VEAF Mission Creation Tools main pipeline
+*/
+
+function buildMission() {
+  //TODO
+}
+
+function extractMission() {
+  //TODO
+}
+
+function normalizeMission() {
+  //TODO
+}
+
+function injectRadioPresets() {
+  //TODO
+}
+
+function injectRealWeather() {
+  // open the DCS mission
+
+  // read the DCS Weather object
+
+  // get METAR from CheckWX
+
+  // process METAR and inject it into the DCS Weather object
+
+  // store the DCS Weather object in the DCS mission
+}

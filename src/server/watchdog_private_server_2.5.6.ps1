cb6b121c2ac5b314ce0a3e9a94797b934b1f7716
David Pierron <dpierron@gmail.com>
2021-04-03 11:42:47 +0200

    increased the initial delay

--- src/server/watchdog_private_server_2.5.6.ps1
+++ src/server/watchdog_private_server_2.5.6.ps1
@@ -5,7 +5,7 @@ $ExePath = "C:\Users\veaf\refresh-opentraining-and-run-dcs.cmd"
 $CommandLine = "private $WindowTitle"
 $WebHookOnFailure = "C:\Users\veaf\webhookAlerter.ps1"
 $Priority = "HIGH"
-$InitialDelay = 30
+$InitialDelay = 60
 $WatchdogDelay = 30
 
 C:/Users/veaf/watchdogProcess.ps1 -ProcessName $ProcessName -WindowTitle $WindowTitle -ExePath $ExePath -CommandLine $CommandLine -WebHookOnFailure $WebHookOnFailure -Priority $Priority -LogfileName $LogfileName -InitialDelay $InitialDelay -WatchdogDelay $WatchdogDelay

ace82d9436b05899650171cc77b722a70843f157
David Pierron <dpierron@gmail.com>
2021-04-13 11:11:53 +0200

    removed server code that went into its own repo

--- src/server/watchdog-test.ps1
+++
@@ -1,9 +0,0 @@
-$ProcessName = "DCS"
-$WindowTitle = "private_server_2.5.6"
-$ExePath = "notepad.exe"
-$CommandLine = " test.txt"
-$WebHookOnFailure = "C:\Users\veaf\webhookAlerter.ps1"
-$Affinity = 5 # cores 0 and 2
-$Priority = "HIGH"
-
-C:/Users/veaf/watchdogProcess.ps1 -ProcessName $ProcessName -WindowTitle $WindowTitle -ExePath $ExePath -CommandLine $CommandLine -WebHookOnFailure $WebHookOnFailure -Priority $Priority -Affinity $Affinity

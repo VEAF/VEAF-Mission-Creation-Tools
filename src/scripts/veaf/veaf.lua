a87b56f258ba0a933ac6b97fa53e93e220641a3c
RexAttaque <tristanmontagnier@gmail.com>
2022-07-15 20:14:49 +0200

    fixed an error with readyForCombat not having a default value for alarm

--- src/scripts/veaf/veaf.lua
+++ src/scripts/veaf/veaf.lua
@@ -32,7 +32,7 @@ veaf = {}
 veaf.Id = "VEAF"
 
 --- Version.
-veaf.Version = "1.21.1"
+veaf.Version = "1.21.2"
 
 --- Development version ?
 veaf.Development = true
@@ -1205,11 +1205,19 @@ function veaf.moveGroupAt(groupName, leadUnitName, heading, speed, timeInSeconds
     return true
 end
 
+veaf.defaultSpawnedAlarmState = 2
+
 function veaf.readyForCombat(group, alarm)
     if type(group) == 'string' then
         group = Group.getByName(group)
     end
     if group then
+
+        local alarm = alarm
+        if not alarm then
+            alarm = veaf.defaultSpawnedAlarmState
+        end
+
         local cont = group:getController()
         cont:setOnOff(true)
         cont:setOption(AI.Option.Ground.id.ALARM_STATE, alarm)	

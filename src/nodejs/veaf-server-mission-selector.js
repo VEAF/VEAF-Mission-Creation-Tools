9b461ba184b9b65ece9d53efdbb627600e5f8d16
David Pierron <dpierron@gmail.com>
2023-01-13 17:25:16 +0100

    v4.64.0

--- src/nodejs/veaf-server-mission-selector.js
+++ src/nodejs/veaf-server-mission-selector.js
@@ -33,6 +33,58 @@ function fitsIn(value, parameter) {
   return value == parameter;
 }
 
+// parses a moment in the day into hours 
+function parseMomentInDay(moment) {
+  if (!moment) return null;
+  switch (moment.toLowerCase()) {
+    case "morning": return "0-11";
+    case "afternoon": return "12-23";
+    case "day": return "07-20";
+    case "night": return "21-06";
+    case "matin": return "0-11";
+    case "après-midi": return "12-23";
+    case "journée": return "07-20";
+    case "nuit": return "21-06";
+  }
+  return null;
+}
+
+// parses a day of week string into a number
+function parseDayOfWeek(dayOfWeek) {
+  if (!dayOfWeek) return null;
+  switch (dayOfWeek.toLowerCase()) {
+    case "monday": return "1";
+    case "tuesday": return "2";
+    case "wednesday": return "3";
+    case "thursday": return "4";
+    case "friday": return "5";
+    case "saturday": return "6";
+    case "sunday": return "7";
+    case "mon": return "1";
+    case "tue": return "2";
+    case "wed": return "3";
+    case "thu": return "4";
+    case "fri": return "5";
+    case "sat": return "6";
+    case "sun": return "7";
+    case "lundi": return "1";
+    case "mardi": return "2";
+    case "mercredi": return "3";
+    case "jeudi": return "4";
+    case "vendredi": return "5";
+    case "samedi": return "6";
+    case "dimanche": return "7";
+    case "lun": return "1";
+    case "mar": return "2";
+    case "mer": return "3";
+    case "jeu": return "4";
+    case "ven": return "5";
+    case "sam": return "6";
+    case "dim": return "7";
+  }
+  return null;
+}
+
 async function selectMission(parameters) {
   let { sourceFileName, targetFileName, configurationFile, trace, quiet } = parameters;
   if (!quiet) console.log(`DCS server startup mission selector starting`);
@@ -70,20 +122,30 @@ async function selectMission(parameters) {
       }
       let selectedMission = data.default;
       for (let i=0; i<data.moments.length; i++) {       
-        let { month, dayOfMonth, dayOfWeek, hour, mission } = data.moments[i];
-        if (month && !(fitsIn(currentMonth, month))) continue;
-        if (dayOfMonth && !(fitsIn(currentDate, dayOfMonth))) continue;
-        if (dayOfWeek && !(fitsIn(currentDay, dayOfWeek))) continue;
-        if (hour && !(fitsIn(currentHour, hour))) continue;
+        if (!data.moments[i]["//comment"]) {
+          let { month, dayOfMonth, dayOfWeek, hour, mission, missions } = data.moments[i];
+          let nDayOfWeek = parseDayOfWeek(dayOfWeek);
+          let nHour = parseMomentInDay(hour);
 
-        // everything fits, select this mission
-        selectedMission = mission;
-        if (!quiet) console.log(`current date ${date.toISOString()} fits in ` + JSON.stringify(data.moments[i]));
-        break;
+          if (month && !(fitsIn(currentMonth, month))) continue;
+          if (dayOfMonth && !(fitsIn(currentDate, dayOfMonth))) continue;
+          if (nDayOfWeek && !(fitsIn(currentDay, nDayOfWeek))) continue;
+          if (nHour && !(fitsIn(currentHour, nHour))) continue;
+
+          // everything fits, select this mission
+          if (mission) {
+            selectedMission = mission;
+          } else if (missions) {
+            // select a random mission in the missions list
+            selectedMission = missions[Math.floor(Math.random() * missions.length)];
+          }
+          if (!quiet) console.log(`current date ${date.toISOString()} fits in ` + JSON.stringify(data.moments[i]));
+          break;
+        }
       }
 
       // edit the file
-      if (!quiet) console.log(`selecting mission ` + selectedMission);
+      if (!quiet) console.log(`\n********************\nselecting mission ` + selectedMission+ `\n********************\n`);
       let sourceFileData = fs.readFileSync(sourceFileName, {encoding:'utf8', flag:'r'});
       let matchResult = sourceFileData.match(`[^\\[]*\\[(\\d+)\\].*${selectedMission}.*`);
       let missionNumber = "01";

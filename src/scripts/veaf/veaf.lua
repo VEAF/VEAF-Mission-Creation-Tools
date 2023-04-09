4ee91f3569359577d1c60a26e3dd659bba6effcd
David Pierron <dpierron@gmail.com>
2023-04-09 19:44:39 +0200

    added veaf.fixUnitsTable and  veaf.isUnitInZone

--- src/scripts/veaf/veaf.lua
+++ src/scripts/veaf/veaf.lua
@@ -19,7 +19,7 @@ veaf = {}
 veaf.Id = "VEAF"
 
 --- Version.
-veaf.Version = "1.41.0"
+veaf.Version = "1.42.0"
 
 --- Development version ?
 veaf.Development = true
@@ -1012,6 +1012,79 @@ function veaf.findPointInZone(spawnSpot, dispersion, isShip)
     else
         return unitPosition
     end
+end
+
+---Fixes a table of mixed units and unit names and returns a table of DCS units
+---@param unitsOrNames table a list of units, unit names, or a mix
+---@return table the DCS units
+function veaf.fixUnitsTable(unitsOrNames)
+	local units = {}
+    for _, unitOrName in pairs(unitsOrNames) do
+        local unit = nil
+        if type(unitOrName) == "table" then
+            -- already an unit
+            unit = unitOrName
+        elseif type(unitOrName) == "string" then
+            -- find by name
+            unit = Unit.getByName(unitOrName) or StaticObject.getByName(unitOrName)
+        end
+        if unit then
+            table.insert(units, unit)
+        end
+    end
+    return units
+end
+
+---checks if a unit is in a trigger zone
+---@param unitOrName any a DCS unit or an unit name
+---@param zoneOrName any a DCS trigger zone or a trigger zone name (any type)
+---@return boolean true if the unit is in the trigger zone
+function veaf.isUnitInZone(unitOrName, zoneOrName)
+    veaf.loggers.get(veaf.Id):debug("veaf.isUnitInZone(unitOrName=%s, zoneOrName=%s)", veaf.p(unitOrName), veaf.p(zoneOrName))
+    local result = false
+    local unit = nil
+    if unitOrName then
+        if type(unitOrName) == "table" then
+            -- already an unit
+            unit = unitOrName
+        elseif type(unitOrName) == "string" then
+            -- find by name
+            unit = Unit.getByName(unitOrName) or StaticObject.getByName(unitOrName)
+        end
+    end
+
+    local zone = nil
+    if zoneOrName then
+        if type(zoneOrName) == "table" then
+            -- already a DCS zone
+            zone = zoneOrName
+        elseif type(zoneOrName) == "string" then
+            -- find by name
+            zone = veaf.getTriggerZone(zoneOrName)
+        end
+    end
+    veaf.loggers.get(veaf.Id):trace("unit=%s", veaf.p(unit))
+    veaf.loggers.get(veaf.Id):trace("zone=%s", veaf.p(zone))
+    if zone and unit then
+        local unitPosition = unit:getPosition().p
+        veaf.loggers.get(veaf.Id):trace("unitPosition=%s", veaf.p(unitPosition))
+        local unitCategory = unit:getCategory()
+        veaf.loggers.get(veaf.Id):trace("unitCategory=%s", veaf.p(unitCategory))
+        if unitPosition and ((unitCategory == 1 and unit:isActive() == true) or unitCategory ~= 1) then -- it is a unit and is active or it is not a unit
+            if zone.verts  then
+                if mist.pointInPolygon(unitPosition, zone.verts) then
+                    result = true
+                end
+
+            else
+                if ((unitPosition.x - zone.x)^2 + (unitPosition.z - zone.y)^2)^0.5 <= zone.radius then
+                    result = true
+                end
+            end
+        end
+    end
+
+    return result
 end
 
 --- TODO doc

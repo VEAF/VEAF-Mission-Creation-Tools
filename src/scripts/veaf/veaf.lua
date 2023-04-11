a0f7003eb2e9709937c0c8ac214a476c1b02da3c
RexAttaque <tristanmontagnier@gmail.com>
2023-04-11 20:13:17 +0200

    completed convertSpeeds support method

--- src/scripts/veaf/veaf.lua
+++ src/scripts/veaf/veaf.lua
@@ -948,49 +948,152 @@ end
 ---@param ktas number? the true airspeed in knots
 ---@param altitude any in feet, defaults to 10000
 ---@param temperature any in celsius, defaults to ISA temperature at altitude
+---@param pressure any in pa, defaults to ISA temperature at altitude
 ---@return table result containing KTAS, KIAS, Mach, IAS_ms and TAS_ms
-function veaf.convertSpeeds(mach, kias, ktas, altitude, temperature)
+function veaf.convertSpeeds(mach, kias, ktas, altitude, temperature, pressure)
     local result = {
-        KTAS = ktas,
-        KIAS = kias,
-        Mach = 0
+        KTAS = 0,
+        KIAS = 0,
+        Mach = 0,
     }
+
+    local h_tropopause = 11000 --m, tropopause start altitude
+    local ft_2_m = 0.30488
+
     local altitude = altitude
     if not altitude then
         altitude = 10000 -- default to 10000 ft
     end
 
+    altitude = altitude*ft_2_m --convert 2 meters
+
+    local T0 = 288.15 --K, ISA+0 altitude, may need to be corrected for mission ground temp
+    local Tz = -0.0065 --K/m, ISA temperature gradient in troposphere
+    local T_tropopause = 216.85 --K, temperature at the border between tropopause and troposphere (temperature in the tropopause)
+    local P0 = 101325 --Pa, standard pressure
+    local Gamma = 1.4 --Air heat capacity ratio
+    local r = 287.03 --J/kg/K Perfect Gas constant for air
+    local g = 9.81 --m/s^2 gravity constant on earth, might need to account for which planet ED is on
+
     local temperature = temperature
     if not temperature then
         -- compute ISA temperature based on altitude
-        temperature = 15 - (1.98 * altitude / 1000)
+        if altitude<h_tropopause then
+            temperature = T0 - Tz*altitude --troposphere (temp in K)
+        else
+            temperature = T_tropopause --tropopause (max altitude 66000ft) (temp in K)
+        end
+    else
+        temperature = temperature + 273.15 --conversion to Kelvin
     end
+
+    local pressure = pressure
+    if not pressure then
+        -- compute pressure based on altitude and ISA temperature
+        if altitude<h_tropopause then
+            pressure = P0*(1+temperature/T0)^(-g/(r*Tz))
+        else
+            pressure = P0*(1+T_tropopause/T0)^(-g/(r*Tz)) * math.exp(-g*(altitude-h_tropopause)/(r*T_tropopause))
+        end
+    end
+
+    ---comment
+    ---@param temperature number temperature in K
+    ---@return number speed of sound in m/s
+    local function speedOfSound(temperature)
+        return math.sqrt(Gamma*r*temperature)
+    end
+
+    local B = Gamma/(Gamma-1)
+
+    ---comment
+    ---@param mach number mach number to calculate (Pt-Ps)/Ps with Pt/Ps given by isentropic relations (NOTE : (Pt-Ps)=deltaP)
+    ---@return number returns the ratio deltaP/P (DPP) (what a pitot tube would measure for M<1)
+    local function isentropicDPP(mach)
+        return (1+(Gamma-1)*mach^2/2)^B-1;
+    end
+
+    ---comment
+    ---@param mach number mach number to calculate (Pt-Ps)/Ps after a normal shock (M>1) (NOTE : (Pt-Ps)=deltaP)
+    ---@return number returns the ratio deltaP/P (DPP) after the normal shock (what a pitot tube would measure for M>1)
+    local function lord_rayleighDPP(mach)
+        local A = ((Gamma+1)*mach^2/2)^B
+        local C = ((Gamma+1)/(2*Gamma*mach^2-Gamma+1))^(B/Gamma);
+        return A*C-1;
+    end
+
+    ---comment
+    ---@param mach1 number the starting mach (mach_0 or mach_p) which determines the deltaP/P1 being computed (for a pitot tube at sea level, subscript 0 (IAS) or at altitude (TAS), subscript p)
+    ---@param getIAS boolean if true, switches to conversion mode from IAS to TAS
+    ---@return number so if you provide only mach_P (TAS), this will return mach_0 (IAS), and if you provide mach_0 and getTAS true (IAS), this will return mach_P (TAS)
+    local function getConvertedMach(mach1, getTAS)
+        local DPP1 = 0;
+        if mach1 > 1 then
+            DPP1 = lord_rayleighDPP(mach1); --At this point it's still deltaP / Pp (DPPP) (subscript p = at pitot tube, subscript 0 = at sea level)
+        else
+            DPP1 = isentropicDPP(mach1); --At this point it's still deltaP / Pp (DPPP) (subscript p = at pitot tube, subscript 0 = at sea level)
+        end
+        
+        if getTAS then
+            DPP1 = P0*DPP1/pressure --conversion from DPP0 to DPPP
+        else
+            DPP1 = pressure*DPP1/P0 --conversion from DPPP to DPP0
+        end
+
+        local mach2 = 1
+
+        local function converge_2_DPP(machStep)
+            while(lord_rayleighDPP(mach2) < DPP1) do --DPP2 = lord_rayleighDPP(mach2)
+                mach2 = mach2+machStep
+            end
+
+            return mach2
+        end
+
+        if DPP1 > lord_rayleighDPP(1) then
+
+            mach2 = converge_2_DPP(0.25) - 0.25 --coarse
+            mach2 = converge_2_DPP(0.0125) - 0.0125 --medium
+            mach2 = converge_2_DPP(0.00625) --fine
+
+        else
+            mach2 = math.sqrt(2*((DPP1+1)^(1/B)-1)/(Gamma-1))
+        end
+
+        return mach2
+    end
+
+    local ms_2_kt = 1.94384
+    local a1 = speedOfSound(temperature)
+    local a0 = speedOfSound(T0)
 
     if mach then
         -- compute speeds from mach number
         result.Mach = mach
 
-        result.KTAS = 39 * mach * math.sqrt(temperature + 273.15) -- temperature in the formula is in Kelvin
-        result.TAS_ms = result.KTAS / 1.94384
+        result.TAS_ms = mach * a1
+        result.KTAS = result.TAS_ms * ms_2_kt
 
-        result.KIAS = 0 -- TODO later
+        result.IAS_ms = getConvertedMach(result.Mach)*a0
+        result.KIAS = result.IAS_ms * ms_2_kt
     elseif kias then
         -- compute speeds from ias
         result.KIAS = kias
-        result.IAS_ms = result.KIAS / 1.94384
+        result.IAS_ms = result.KIAS / ms_2_kt
 
-        result.KTAS = kias * (1 + (altitude / 1000 * .02)) -- TODO do better
-        result.TAS_ms = result.KTAS / 1.94384
+        result.TAS_ms = getConvertedMach(result.IAS_ms/a0, true)*a1
+        result.KTAS = result.TAS_ms * ms_2_kt
 
-        result.Mach = result.KTAS / (39 * math.sqrt(temperature + 273.15)) -- temperature in the formula is in Kelvin
+        result.Mach = result.TAS_ms / a1
     elseif ktas then
         -- compute speeds from tas
         result.KTAS = ktas
-        result.TAS_ms = result.KTAS / 1.94384
+        result.TAS_ms = result.KTAS / ms_2_kt
 
-        result.KIAS = 0 -- TODO later
+        result.Mach = result.TAS_ms / a1
 
-        result.Mach = result.KTAS / (39 * math.sqrt(temperature + 273.15)) -- temperature in the formula is in Kelvin
+        result.IAS_ms = getConvertedMach(result.Mach)*a0
+        result.KIAS = result.IAS_ms * ms_2_kt
     end
     return result
 end

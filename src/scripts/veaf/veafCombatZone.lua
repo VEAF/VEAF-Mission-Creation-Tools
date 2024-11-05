44dfdb5044156094939254469a0d23699d5e1e35
Guillaume Duparquet <gduparquet@gmail.com>
2024-11-05 14:35:20 +0100

    chore: increment module version

--- src/scripts/veaf/veafCombatZone.lua
+++ src/scripts/veaf/veafCombatZone.lua
@@ -20,7 +20,7 @@ veafCombatZone = {}
 veafCombatZone.Id = "COMBATZONE"
 
 --- Version.
-veafCombatZone.Version = "1.16.0"
+veafCombatZone.Version = "1.16.1"
 
 -- trace level, specific to this module
 --veafCombatZone.LogLevel = "trace"

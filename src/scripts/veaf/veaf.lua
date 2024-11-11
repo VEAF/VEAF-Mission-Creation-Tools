c3f32cbf7866e30b2e50cbd0645e0a1ef01ce375
David Pierron <dpierron@gmail.com>
2024-11-11 18:05:36 +0100

    veaf.p: added metadata (e.g., functions) and default DCS functions are called

--- src/scripts/veaf/veaf.lua
+++ src/scripts/veaf/veaf.lua
@@ -19,7 +19,7 @@ veaf = {}
 veaf.Id = "VEAF"
 
 --- Version.
-veaf.Version = "1.49.1"
+veaf.Version = "1.50.0"
 
 --- Development version ?
 veaf.Development = false
@@ -600,7 +600,7 @@ function veaf.isNullOrEmpty(s)
     return (s == nil or (type(s) == "string" and s == ""))
 end
 
-function veaf.p(o, level, skip)
+function veaf.p(o, level, skip, includeMeta, dontRecurse)
     if o and type(o) == "table" and (o.x and o.z and o.y and #o == 3) then
         return string.format("{x=%s, z=%s, y=%s}", veaf.p(o.x), veaf.p(o.z), veaf.p(o.y))
     elseif o and type(o) == "table" and (o.x and o.y and #o == 2)  then
@@ -612,10 +612,10 @@ function veaf.p(o, level, skip)
             skip[value]=true
         end
     end
-    return veaf._p(o, level, skip)
+    return veaf._p(o, level, skip, includeMeta, dontRecurse)
 end
 
-function veaf._p(o, level, skip)
+function veaf._p(o, level, skip, includeMeta, dontRecurse)
     local MAX_LEVEL = 20
     if level == nil then level = 0 end
     if level > MAX_LEVEL then
@@ -625,7 +625,7 @@ function veaf._p(o, level, skip)
     local text = ""
     if o == nil then
         text = "[nil]"
-    elseif (type(o) == "table") then
+    elseif (type(o) == "table") and not(dontRecurse) then
         text = "\n"
         local keys = {}
         local values = {}
@@ -641,9 +641,43 @@ function veaf._p(o, level, skip)
                 text = text .. " "
             end
             if not (skip and skip[key]) then
-                text = text .. ".".. key.."="..veaf.p(value, level+1, skip) .. "\n"
+                text = text .. ".".. key.."="..veaf.p(value, level+1, skip, includeMeta, dontRecurse) .. "\n"
             else
                 text = text .. ".".. key.."= [[SKIPPED]]\n"
+            end
+        end
+        if includeMeta then
+            local metatable = getmetatable(o)
+            if metatable then
+                text = "\n"
+                local keys = {}
+                local values = {}
+                for key, value in pairs(metatable) do
+                    local sKey = tostring(key)
+                    table.insert(keys, sKey)
+                    values[sKey] = value
+                end
+                table.sort(keys)
+                for _, key in pairs(keys) do
+                    local value = values[key]
+                    for i=0, level do
+                        text = text .. " "
+                    end
+                    if not (skip and skip[key]) then
+                        if key == "getID" then
+                            value = o:getID()
+                        elseif key == "getName" then
+                            value = o:getName()
+                        elseif key == "getTypeName" then
+                            value = o:getTypeName()
+                        elseif key == "getDesc" then
+                            value = o:getDesc()
+                        end
+                        text = text .. "[META].".. key.."="..veaf.p(value, level+1, skip, includeMeta, true) .. "\n"
+                    else
+                        text = text .. "[META].".. key.."= [[SKIPPED]]\n"
+                    end
+                end
             end
         end
     elseif (type(o) == "function") then
